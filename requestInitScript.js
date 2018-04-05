72cf3edcc90f161598ff4e556820c19a5b4801ea
shailee mehta <mehtashailee21@gmail.com>
2018-04-05 11:43:24 +0530

    sockhangup handled

--- requestInitScript.js
+++ requestInitScript.js
@@ -1,7 +1,143 @@
+// process.env.UV_THREADPOOL_SIZE = 128;
 var request = require("request");
+// request.debug = true;
 var async = require("async");
+const querystring = require('querystring');
+const http = require('http');
+var httpAgent = new http.Agent({ keepAlive: true, maxSockets: 1 });
+// var agent = new http.Agent;
+// agent.maxSockets = 1;
+// agent.setKeepAlive=true;
+
+function sendRequestunirest(url, postbody, timeout, cb) {
+    var path = url.split("?");
+
+    var req = unirest("POST", { url: path[0], timeout: timeout, forever: true });
+    path.splice(0, 1)
+    path.forEach(function(e, i) {
+        req.query(e);
+    })
+
+
+    req.headers({
+
+        "content-type": "application/json"
+    });
+
+    req.type("json");
+    req.send(postbody);
+
+    req.end(function(res) {
+        console.log(JSON.stringify(res))
+        if (res.error) {
+            var finalResponse = {};
+            finalResponse[url] = {
+                response: {
+                    statusCode: res.statusCode,
+                    error: res.error
+                },
+                headers: res.headers
+            }
+            cb({ status: false, content: finalResponse })
+
+        } else {
+            var finalResponse = {};
+            finalResponse[url] = {
+                response: res.body ? res.body : null,
+                headers: res.headers
+            }
+            cb({ status: true, content: finalResponse })
+
+        }
+
+    });
+
+}
 
 function sendRequest(url, postbody, timeout, cb) {
+    var postData = JSON.stringify(postbody);
+    // var postData = querystring.stringify(postbody);
+    console.log(url)
+    var path = url.split("/");
+    path = path.splice(3, path.length - 1)
+    var options = {
+        protocol: url.split("/")[0],
+        hostname: url.split("/")[2],
+        port: null,
+        path: "/" + path.join("/"),
+        method: 'POST',
+        // setKeepAlive:true,
+        agent: httpAgent, //agent,//httpAgent,
+        // timeout: timeout,
+        headers: {
+            'content-type': 'application/json',
+            'connection': 'keep-alive',
+            // 'content-length': postData.length
+        }
+    };
+    var req = http.request(options, (res) => {
+        console.log('statusCode:', res.statusCode);
+        // res.setTimeout(timeout)
+        if (res.statusCode == 200) {
+            res.on('data', (d) => {
+                var finalResponse = {};
+                finalResponse[url] = {
+                    response: d ? d.toString() : null,
+                    headers: res.headers
+                }
+                cb({ status: true, content: finalResponse })
+
+            });
+        } else if (res.statusCode == 204) {
+            var finalResponse = {};
+            finalResponse[url] = {
+                response: null,
+                headers: res.headers
+            }
+            cb({ status: true, content: finalResponse })
+        } else {
+            var finalResponse = {};
+            finalResponse[url] = {
+                response: {
+                    statusCode: res.statusCode
+                },
+                headers: res.headers
+            }
+            cb({ status: false, content: finalResponse })
+        }
+
+    }).setTimeout(timeout)
+    req.on('timeout', (s) => {
+
+        // var finalResponse = {};
+        // finalResponse[url] = {
+        //     response: "timedout",
+        // }
+        // cb({ status: false, content: finalResponse })
+        req.abort();
+    });
+
+    req.setSocketKeepAlive(true);
+    //req.timeout=timeout
+    req.on('error', (error) => {
+        console.log("error ::: ", error.code)
+        var finalResponse = {};
+        finalResponse[url] = {
+            response: { error: error.code }
+        }
+        cb({ status: true, content: finalResponse })
+
+    });
+
+    req.write(postData);
+    req.end();
+
+
+
+}
+
+function sendRequestReq(url, postbody, timeout, cb) {
+    var pool = new https.Agent({ keepAlive: true });
     var options = {
         method: 'POST',
         url: url,
@@ -10,11 +146,13 @@ function sendRequest(url, postbody, timeout, cb) {
         headers: {
             'content-type': 'application/json'
         },
+
         body: postbody,
         json: true
     };
 
     request(options, function(error, response, body) {
+        console.log(error, response.statusCode)
         if (error) {
             cb({ status: false, content: error })
         } else {
@@ -44,29 +182,45 @@ function init(urls, request, timeoutInMS, cb) {
             response: {}
         };
         async.each(Object.keys(urls), function(recipient, callbk) {
+            console.log("Requested At ::", recipient, ' ', new Date(), "\n");
             sendRequest(urls[recipient], body, timeoutInMS, function(resp) {
+                console.log("recieved at ", recipient, ' ', new Date(), "\n");
+
+                if (!responseArray.headers.hasOwnProperty(recipient)) {
+                    responseArray.headers[recipient] = []
+                }
+                if (!responseArray.response.hasOwnProperty(recipient)) {
+                    responseArray.response[recipient] = []
+                }
+                // console.log(resp.content[urls[recipient]],recipient,urls[recipient])
+
+                if (!resp.content[urls[recipient]].headers) {
+                    resp.content[urls[recipient]].headers = {}
+                }
+                resp.content[urls[recipient]].headers["url"] = urls[recipient]
+
+                // }
+                if (!resp.content[urls[recipient]].response ) {
+                    resp.content[urls[recipient]].response = {}
+                }
+                    resp.content[urls[recipient]].response["url"] = urls[recipient]
+                // }
+                responseArray.headers[recipient].push(resp.content[urls[recipient]].headers)
+                responseArray.response[recipient].push(resp.content[urls[recipient]].response)
+
+
+
                 if (!resp.status) {
                     callbk(resp)
                 } else {
-                    if (!responseArray.headers.hasOwnProperty(recipient)) {
-                        responseArray.headers[recipient] = []
-                    }
-                    if (!responseArray.response.hasOwnProperty(recipient)) {
-                        responseArray.response[recipient] = []
-                    }
-                    resp.content[urls[recipient]].headers["url"] = urls[recipient]
-                    if (resp.content[urls[recipient]].response !== null) {
-                        resp.content[urls[recipient]].response["url"] = urls[recipient]
-                    }
-                    responseArray.headers[recipient].push(resp.content[urls[recipient]].headers)
-                    responseArray.response[recipient].push(resp.content[urls[recipient]].response)
 
                     callbk();
                 }
             })
         }, function(e) {
+            console.log(e)
             if (e) {
-                cb(e)
+                cb(responseArray)
                 return;
             }
             cb(responseArray)
